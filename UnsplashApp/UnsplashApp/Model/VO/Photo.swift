63c9c04d758e414be632e96f592c696b71ff17e5
eunjeongS2 <dmswjd2340@gmail.com>
2021-01-09 00:00:52 +0900

    [feat] Photo VO 생성

---
+++ UnsplashApp/UnsplashApp/Model/VO/Photo.swift
@@ -0,0 +1,14 @@
+//
+//  Photo.swift
+//  UnsplashApp
+//
+//  Created by eunjeong lee on 2021/01/08.
+//
+
+import Foundation
+
+struct Photo {
+    let id: String
+    let urls: PhotoURL
+    let user: User
+}
