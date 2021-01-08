7a48053294c5901c3c7e419f1f6c51f6d38a62bf
eunjeongS2 <dmswjd2340@gmail.com>
2021-01-08 17:19:33 +0900

    [feat] ImageCollectionViewCell 커스텀 클래스 생성, xib에서 IBOutlet 연결

---
+++ UnsplashApp/UnsplashApp/View/ImageCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCollectionViewCell.swift
+//  UnsplashApp
+//
+//  Created by eunjeong lee on 2021/01/08.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+}
