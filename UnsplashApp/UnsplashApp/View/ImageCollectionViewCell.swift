d6ac957eafbb2eb2a746acb69eecb86c5c93e288
eunjeongS2 <dmswjd2340@gmail.com>
2021-01-08 17:52:19 +0900

    [feat] ImageCollectionViewCell register 함수 추가

--- UnsplashApp/UnsplashApp/View/ImageCollectionViewCell.swift
+++ UnsplashApp/UnsplashApp/View/ImageCollectionViewCell.swift
@@ -11,4 +11,25 @@ class ImageCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet private weak var imageView: UIImageView!
     
+    
+}
+
+extension ImageCollectionViewCell {
+    
+    private static var typeName: String {
+        String(describing: type(of: self))
+    }
+    
+    static func registerNib(collectionView: UICollectionView) {
+        let nib = UINib(nibName: ImageCollectionViewCell.typeName, bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: Identifier.reusableCell)
+    }
+    
+}
+
+private extension ImageCollectionViewCell {
+    
+    enum Identifier {
+        static let reusableCell: String = "ImageCell"
+    }
 }
