413425f379728f4e1db1e11e6275b3ce2ad8a622
eunjeongS2 <dmswjd2340@gmail.com>
2021-01-11 18:35:43 +0900

    [feat] DetailCollectionViewCell에 기존 DetailView의 애니메이션, 이미지뷰 설정부분 이동

--- UnsplashApp/UnsplashApp/View/DetailCollectionViewCell.swift
+++ UnsplashApp/UnsplashApp/View/DetailCollectionViewCell.swift
@@ -11,5 +11,42 @@ class DetailCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet private weak var imageView: UIImageView!
     @IBOutlet private weak var imageViewCenterYConstraint: NSLayoutConstraint!
+    private var imageHeight: CGFloat = .zero
     
+    func configureView(image: UIImage?) {
+        imageView.image = image
+        guard let image = image else { return }
+        
+        imageHeight = image.size.height
+    }
+    
+    func animate(startY: CGFloat) {
+        let differ = startY - (frame.size.height - (imageHeight / 2)) / 2
+        imageViewCenterYConstraint.constant = differ
+        layoutIfNeeded()
+
+        UIView.animate(
+            withDuration: 0.5,
+            delay: 0,
+            usingSpringWithDamping: 0.85,
+            initialSpringVelocity: 0.5,
+            options: .curveEaseIn) {
+            
+           self.imageViewCenterYConstraint.constant = 0
+           self.layoutIfNeeded()
+       }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+        imageHeight = .zero
+    }
+}
+
+extension DetailCollectionViewCell {
+    
+    enum Identifier {
+        static let reusableCell: String = "DetailPhotoCell"
+    }
 }
