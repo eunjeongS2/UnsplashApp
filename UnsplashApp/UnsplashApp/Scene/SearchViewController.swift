cb19b8115660a24e1a1bccfe10ff15ebe489d259
eunjeongS2 <dmswjd2340@gmail.com>
2021-01-13 03:43:19 +0900

    [feat] SearchViewController 레이아웃 구현

---
+++ UnsplashApp/UnsplashApp/Scene/SearchViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SearchViewController.swift
+//  UnsplashApp
+//
+//  Created by eunjeong lee on 2021/01/13.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBOutlet private weak var searchBar: UISearchBar!
+    @IBOutlet private weak var photoCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func cancelButtonTouched(_ sender: UIButton) {
+        
+    }
+    
+}
